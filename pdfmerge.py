56c8da0d14ecc57fd58cc083f779262ab6672d68
Etienne <elescarbeault@gmail.com>
2020-11-13 16:33:44 -0500

    manual lint fix

--- pdfmerge.py
+++ pdfmerge.py
@@ -1,66 +1,75 @@
 import os
 import sys
-import time
 from PyPDF2 import PdfFileReader, PdfFileMerger
+
 
 def getPDFFiles():
     # return an array of constructed file objects
     # based on pdfs in current repository
     current_dir = sys.argv[1]
     pdf_file_names = []
-    pdf_file_names += [each for each in os.listdir(current_dir) 
-        if (each.endswith('.pdf') or each.endswith('.PDF'))
-    ]
+    pdf_file_names += [each for each in os.listdir(current_dir)
+                       if (each.endswith('.pdf') or each.endswith('.PDF'))]
 
     return pdf_file_names
 
 
-def printPDFList(file_names:str = []):
-    # Prints list 
+def printPDFList(file_names: str = []):
+    # Prints list
     # Generates a formatted list in the console
     out = ""
     for i in range(len(file_names)):
-        out += '('+ str(i+1) + ') ' + file_names[i] + "   " # 3 spaces
-        if((i+1)%3 == 0):
+        out += '(' + str(i+1) + ') ' + file_names[i] + "   "  # 3 spaces
+        if((i + 1) % 3 == 0):
             out += "\n"
     print(out)
 
-def getConcatFiles(index_group:int=[], file_names:str=[]):
+
+def getConcatFiles(index_group: int = [], file_names: str = []):
     merged_file = PdfFileMerger()
     for i in index_group:
-        fd = open(file_names[i], 'rb') # Add error control
-        merged_file.append(PdfFileReader(fd))
-
+        try:
+            fd = open(file_names[i], 'rb')  # Add error control
+            merged_file.append(PdfFileReader(fd))
+        except Exception:
+            print("Error: Could not open " + file_names[i])
+            print("It will be ignored in the merging process. \n")
     return merged_file
 
-def getFileIndicesInput(fileArrayLen:int):
+
+def getFileIndicesInput(fileArrayLen: int):
     index_arr = []
     isValid = False
-    while not isValid: 
+    while not isValid:
         index_seq = input("Sequence of files to merge \n").split()
         try:
             for i in index_seq:
+                if i > fileArrayLen:
+                    raise Exception()
+
                 print(i)
                 index_arr.append(int(i)-1)
             isValid = True
-        except:
-            print("Invalid input")
+        except Exception:
+            print("""Invalid input - Make sure to enter a valid
+            number sequence separated by spaces\n""")
             isValid = False
-        
     return index_arr
 
 
-def handleConcatFile(mergedFile:PdfFileMerger):
+def handleConcatFile(mergedFile: PdfFileMerger):
     output_name = input("Output name: \n")
     mergedFile.write(output_name + ".pdf")
     mergedFile.close()
 
-# Main script
+
+'''
+Main script
+'''
 files = getPDFFiles()
 files_length = len(files)
 if len(files) <= 0:
-    print("No PDF file found! Exiting...")
-    time.sleep(5)
+    print("No PDF file found! Exiting...\n")
     sys.exit()
 
 
